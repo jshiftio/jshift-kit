0e435510a52d6aa9fcbf8ce116c80a1090b6f7e4
Rohan Kumar <rohaan@redhat.com>
2019-04-29 23:51:53 +0530

    Initial commit

---
+++ build/api/src/main/java/io/jshift/kit/build/api/RegistryContext.java
@@ -0,0 +1,21 @@
+package io.jshift.kit.build.api;
+
+import java.io.IOException;
+
+import io.jshift.kit.build.api.auth.RegistryAuth;
+import io.jshift.kit.build.api.auth.RegistryAuthConfig;
+import io.jshift.kit.config.image.build.ImagePullPolicy;
+
+/**
+ * @author roland
+ * @since 17.10.18
+ */
+public interface RegistryContext {
+
+    ImagePullPolicy getDefaultImagePullPolicy();
+
+    String getRegistry(RegistryAuthConfig.Kind kind);
+
+    RegistryAuth getAuthConfig(RegistryAuthConfig.Kind kind, String user, String registry) throws IOException;
+
+}
